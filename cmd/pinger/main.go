5576ebabaa0eca4a2ae584adab19b4d806935f2f
zapi <screbov2@gmail.com>
2026-04-28 20:41:30 +0200

    feat: add main function with graceful shutdown

--- cmd/pinger/main.go
+++ cmd/pinger/main.go
@@ -1,7 +1,108 @@
 package main
 
-import "github.com/Zapi-web/gopher-pinger/internal/storage/local"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	handDelete "github.com/Zapi-web/gopher-pinger/internal/api/handlers/delete"
+	"github.com/Zapi-web/gopher-pinger/internal/api/handlers/get"
+	logMiddleware "github.com/Zapi-web/gopher-pinger/internal/api/handlers/middleware"
+	"github.com/Zapi-web/gopher-pinger/internal/api/handlers/post"
+	"github.com/Zapi-web/gopher-pinger/internal/api/handlers/put"
+	"github.com/Zapi-web/gopher-pinger/internal/config"
+	"github.com/Zapi-web/gopher-pinger/internal/logger"
+	"github.com/Zapi-web/gopher-pinger/internal/metrics"
+	"github.com/Zapi-web/gopher-pinger/internal/service"
+	"github.com/Zapi-web/gopher-pinger/internal/storage/database"
+	"github.com/Zapi-web/gopher-pinger/internal/storage/local"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
 
 func main() {
-	mapStr := local.InitMap()
+	cfg, err := config.Init()
+
+	if err != nil {
+		slog.Error("failed to read config", "err", err)
+		os.Exit(1)
+	}
+
+	slog.SetDefault(logger.New(cfg.LogLevel))
+	slog.Info("logger initialized", "lvel", cfg.LogLevel)
+
+	appCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	state, err := database.New(cfg.Addr)
+
+	if err != nil {
+		slog.Error("failed to connect to database", "err", err)
+		os.Exit(1)
+	}
+	defer state.Close()
+
+	processes := local.InitMap()
+	promMetrics := metrics.New()
+
+	metricsInterface := service.NewMetricsService(*promMetrics)
+	controlInterface := service.NewService(processes, state, promMetrics)
+
+	err = controlInterface.Init(appCtx)
+
+	if err != nil {
+		slog.Error("failed to init service", "err", err)
+		os.Exit(1)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.RealIP, middleware.Recoverer)
+	r.Use(func(next http.Handler) http.Handler {
+		return logMiddleware.LoggingMiddleware(next, metricsInterface)
+	})
+
+	r.Post("/newPinger", post.New(controlInterface))
+	r.Get("/getPinger", get.New(controlInterface))
+	r.Put("/changeInterval", put.New(controlInterface))
+	r.Delete("/deletePinger", handDelete.New(controlInterface))
+
+	slog.Info("starting server", "addr", cfg.Port)
+
+	srv := &http.Server{
+		Addr:         ":" + cfg.Port,
+		Handler:      r,
+		ReadTimeout:  4 * time.Second,
+		WriteTimeout: 4 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	serverError := make(chan error, 1)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			serverError <- err
+		}
+	}()
+
+	select {
+	case err := <-serverError:
+		if err != nil {
+			slog.Error("failed to start server", "err", err)
+		}
+	case <-appCtx.Done():
+		slog.Info("Received a signal. Trying graceful shutdown")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			_ = srv.Close()
+			slog.Error("Could not stop server gracefully", "err", err)
+		}
+	}
+
+	slog.Info("server stopped")
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,19 @@ module github.com/Zapi-web/gopher-pinger
 go 1.26.1
 
 require (
+	github.com/go-chi/chi v1.5.5
+	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/oklog/ulid/v2 v2.1.1
 	github.com/prometheus/client_golang v1.23.2
 	github.com/redis/go-redis/v9 v9.18.0
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
@@ -21,4 +25,6 @@ require (
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
