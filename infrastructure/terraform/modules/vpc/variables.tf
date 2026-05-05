f3526532847000f06db5d013ae03823bea0b78f1
zapi <screbov2@gmail.com>
2026-05-05 19:34:59 +0200

    feat: add default values to variables

--- infrastructure/terraform/modules/vpc/variables.tf
+++ infrastructure/terraform/modules/vpc/variables.tf
@@ -1,10 +1,12 @@
 variable "app_name" {
   type        = string
+  default = "default-app"
   description = "App name"
 }
 
 variable "environment" {
   type        = string
+  default = "test"
   description = "App environment"
 
   validation {
@@ -22,6 +24,12 @@ variable "subnet_config" {
     type       = string
   }))
 
+  default = {
+    "app_1" = { cidr_block = "10.0.1.0/24", az = "eu-central-1a", type = "app"}
+    "app_2" = { cidr_block = "10.0.2.0/24", az = "eu-central-1b", type = "app"}
+        "db" = { cidr_block = "10.0.3.0/24", az = "eu-central-1a", type = "db"}
+        "obs" = { cidr_block = "10.0.4.0/24", az = "eu-central-1a", type = "obs"}
+  }
   validation {
     condition     = contain(["app", "db", "obs"], var.subnet_config.type)
     error_message = "type must be app, db or obs only"
