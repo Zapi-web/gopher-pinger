f2b34dbcd4b5b84e2b53937a99e899b692447ba0
zapi <screbov2@gmail.com>
2026-05-06 22:03:42 +0200

    feat: add s3 bucket for state

--- infrastructure/terraform/terraform.tf
+++ infrastructure/terraform/terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket         = "amzn-s3-unique-terraform-bucket-271598835315-eu-central-1-an"
+    key            = "terraform-tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "lock-table"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
